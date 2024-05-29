38feacea2922754b1ae6c57e4662b53df74b8e21
Felipe0101 <felipeeduardodealmeida@gmail.com>
2024-05-28 21:50:50 -0300

    feat:add footer

---
+++ src/ui/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="w-full h-[34px] bg-slate-300 text-center">
+      MKS sistemas © Todos os direitos reservados
+    </footer>
+  );
+}
+
+export default Footer;
